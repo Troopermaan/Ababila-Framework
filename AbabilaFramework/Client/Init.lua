f1a3226326e7311ee4214ceba285444201cf1ae9
Trooper <122026624+Troopermaan@users.noreply.github.com>
2024-02-08 23:16:47 -0800

    Create Init.lua

---
+++ AbabilaFramework/Client/Init.lua
@@ -0,0 +1,7 @@
+debug.setmemorycategory('Init.lua')
+
+-- // Client Main
+local LocalPlayer = game:GetService('Players').LocalPlayer
+
+-- // Modules
+local Framework = require(script:WaitForChild('Framework'))
